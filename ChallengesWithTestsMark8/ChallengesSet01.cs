66bff54d7b44b1e67456709004765bb134324e11
Grant Hill <granthill@live.com>
2025-01-07 12:54:27 -0500

    TC Weekly Challenges 01 Assignment Submission.

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -2,7 +2,7 @@
 
 namespace ChallengesWithTestsMark8
 {
-    public class ChallengesSet01
+    public class ChallengesSet01//passed all tests.
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
@@ -18,32 +18,52 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
-        }
+            return minuend - subtrahend;//passed all tests.
+        }//need to ensure that the subtrahend is being subtracted from the minuend.... else, negative values could be produced, and won't pass all of the tests.
 
         public int Add(int number1, int number2)
-        {
-            throw new NotImplementedException();
+        {//passed all tests. For some reason, this exact scope failed the first time I ran the tests on it.
+            return number2 + number1;
         }
 
         public int GetSmallestNumber(int number1, int number2)
-        {
-            throw new NotImplementedException();
+        {//passed all tests. For some reason, this exact scope failed the first time I ran the tests on it.
+            //throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
-        {
-            throw new NotImplementedException();
+        {//passed all tests.
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
-        {
-            throw new NotImplementedException();
+        {//passed all tests. Really had to analyze each test to figure out what this method was looking for as far as passing all of its respective tests.
+            //throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+            //Console.WriteLine($"Hello!" {nameOfPerson});
+            //return Console.WriteLine($"Hello, {nameOfPerson}");
         }
 
         public string GetHey()
-        {
-            throw new NotImplementedException();
+        {//passed alltests. Just remember to look at 
+            //throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
